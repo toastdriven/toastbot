843920850a94afaca21e9b88880a9acc352864f8
Daniel Lindsley <daniel@toastdriven.com>
2012-02-27 10:31:16 -0600

    Bumped to v0.4.1!

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+import os
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
 
 setup(
     name='toastbot',
     version='0.4.1',
     description='A clean, extensible IRC bot using irckit.',
-    long_description=open('README.md', 'r').read(),
+    long_description=open(README, 'r').read(),
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
     py_modules = ['toastbot'],
@@ -18,4 +21,5 @@ setup(
         'Programming Language :: Python',
         'Topic :: Utilities'
     ],
+    url = 'http://github.com/toastdriven/toastbot'
 )
